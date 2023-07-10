7ddf6598dcc3f9cf64a8a0dc40857eaf9388a526
Serhi04uk <Sergey.willian17@gmail.com>
2023-07-10 19:42:00 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+
 import './index.css';
 import { Prof } from 'components/Profile/Profile';
 import { Stat } from 'components/Statistics/Statistics';
@@ -9,7 +9,6 @@ import { Trans } from 'components/Transaction/Transaction';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App />  */}
     <Prof></Prof>
     <Stat></Stat>
     <Friends></Friends>
